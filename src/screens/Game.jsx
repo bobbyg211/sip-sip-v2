258fe6e23202ff589839f2e7e52e665fbd732e16
Robert Gonzalez <me@robertgonzalez.io>
2025-07-21 13:04:26 -0400

    GH pages logic added

--- src/screens/Game.jsx
+++ src/screens/Game.jsx
@@ -9,50 +9,50 @@ import cheesyLg from "../images/cheesy-lg.svg";
 
 export default function Game() {
   // const { action: getPrompts, data: prompts } = useGetPrompt();
-  const [currPrompt, setCurrPrompt] = useState();
-  const [nextPrompt, setNextPrompt] = useState();
+  // const [currPrompt, setCurrPrompt] = useState();
+  // const [nextPrompt, setNextPrompt] = useState();
 
-  function promptTrans() {
-    document.querySelector(".game .prompts .prompt-1").classList.add("next");
-    document.querySelector(".game .prompts .prompt-2").classList.add("next");
-    getPrompts().then((newPrompts) => {
-      setTimeout(() => {
-        setCurrPrompt(nextPrompt);
-      }, 500);
+  // function promptTrans() {
+  //   document.querySelector(".game .prompts .prompt-1").classList.add("next");
+  //   document.querySelector(".game .prompts .prompt-2").classList.add("next");
+  //   getPrompts().then((newPrompts) => {
+  //     setTimeout(() => {
+  //       setCurrPrompt(nextPrompt);
+  //     }, 500);
 
-      setTimeout(() => {
-        document.querySelector(".game .prompts .prompt-1").classList.remove("next");
-        document.querySelector(".game .prompts .prompt-2").classList.remove("next");
-        setNextPrompt(newPrompts[1]?.content);
-      }, 750);
-    });
-  }
+  //     setTimeout(() => {
+  //       document.querySelector(".game .prompts .prompt-1").classList.remove("next");
+  //       document.querySelector(".game .prompts .prompt-2").classList.remove("next");
+  //       setNextPrompt(newPrompts[1]?.content);
+  //     }, 750);
+  //   });
+  // }
 
-  async function shareGame() {
-    try {
-      await navigator.share({
-        title: "SipSip",
-        text: "SipSip: Pour decisions make great stories...",
-        url: "https://playsipsip.com",
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  // async function shareGame() {
+  //   try {
+  //     await navigator.share({
+  //       title: "SipSip",
+  //       text: "SipSip: Pour decisions make great stories...",
+  //       url: "https://playsipsip.com",
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
 
-  const renderHTML = (rawHTML) =>
-    React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
+  // const renderHTML = (rawHTML) =>
+  //   React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
 
-  useEffect(() => {
-    if (!currPrompt || !nextPrompt) {
-      setCurrPrompt(prompts[0]?.content);
-      setNextPrompt(prompts[1]?.content);
-    }
-  }, [prompts]);
+  // useEffect(() => {
+  //   if (!currPrompt || !nextPrompt) {
+  //     setCurrPrompt(prompts[0]?.content);
+  //     setNextPrompt(prompts[1]?.content);
+  //   }
+  // }, [prompts]);
 
   return (
     <div className="game">
-      <div className="prompts">
+      {/* <div className="prompts">
         <div className="prompt prompt-1">
           <p>{renderHTML(currPrompt)}</p>
           <button onClick={promptTrans} type="button" className="primary-btn">
@@ -88,7 +88,7 @@ export default function Game() {
         <img src={sippy} alt="Sippy" id="sippy" />
         <img className="sippy-lg" src={sippyLg} alt="" />
         <img className="cheesy-lg" src={cheesyLg} alt="" />
-      </div>
+      </div> */}
     </div>
   );
 }

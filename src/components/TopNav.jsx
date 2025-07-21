e6df81ffb3e947ca1b073c5e72a450c1631883d9
Robert Gonzalez <me@robertgonzalez.io>
2025-07-21 17:11:07 -0400

    Refactor TopNav

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -11,44 +11,23 @@ export default function TopNav() {
   return (
     <div className="top-nav">
       {isGamePage && (
-        <div
-          className="global-home"
-          style={{ position: "absolute", top: 16, left: 16, zIndex: 1000 }}
-        >
-          <Link
-            to="/"
-            className="home-btn"
-            style={{ background: "none", border: "none", cursor: "pointer" }}
-          >
-            <img src={home} alt="Home" style={{ width: 32, height: 32 }} />
+        <div className="global-home">
+          <Link to="/" className="nav-btn">
+            <img src={home} alt="Home" className="nav-icon" />
           </Link>
         </div>
       )}
       {isSettingsPage && (
-        <div
-          className="global-back"
-          style={{ position: "absolute", top: 16, left: 16, zIndex: 1000 }}
-        >
-          <Link
-            to={-1}
-            className="back-btn"
-            style={{ background: "none", border: "none", cursor: "pointer" }}
-          >
-            <img src={back} alt="Back" style={{ width: 32, height: 32 }} />
+        <div className="global-back">
+          <Link to={-1} className="nav-btn">
+            <img src={back} alt="Back" className="nav-icon" />
           </Link>
         </div>
       )}
       {!isSettingsPage && (
-        <div
-          className="global-settings"
-          style={{ position: "absolute", top: 16, right: 16, zIndex: 1000 }}
-        >
-          <Link
-            to="/settings"
-            className="settings-btn"
-            style={{ background: "none", border: "none", cursor: "pointer" }}
-          >
-            <img src={gear} alt="Settings" style={{ width: 32, height: 32 }} />
+        <div className="global-settings">
+          <Link to="/settings" className="nav-btn">
+            <img src={gear} alt="Settings" className="nav-icon" />
           </Link>
         </div>
       )}
